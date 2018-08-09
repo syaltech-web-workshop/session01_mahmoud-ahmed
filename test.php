3a8cf5aa0fe438ea9dd5176443d17b25701e1543
Mahmoud <sswsy2006@gmail.com>
2018-08-10 01:57:21 +0200

    test_

--- test.php
+++ test.php
@@ -17,12 +17,77 @@
 //
 //echo $herbie->model;
 
-$x = "Hello world!";
+//$x = "Hello world!";
+//
+//$position = strpos($x, ",");
+//
+//echo $position;
+//
+//$y = false;
+//
+//echo $y;
 
-$position = strpos($x, ",");
+//$array = array(1, 2, 3, 4, 5);
+//
+//foreach ($array as $el_value){
+//    echo $el_value . "\n";
+//}
 
-echo $position;
+//function familyName($fname, $year) {
+//    echo "$fname Ahmed, born in $year \n.";
+//}
+//
+//familyName("Mahmoud", 1992);
 
-$y = false;
+//function setHeight($min_height = 50) {
+//    echo "The height is : $min_height \n";
+//}
+//
+//setHeight();
 
-echo $y;
+//function sum() {
+//    $z = 2 + 5;
+//    echo "2 + 5 = $z \n";
+//}
+//
+//var_dump(sum());
+
+$array[2] = 2;
+$array[3] = 3;
+
+
+//var_dump(count($array));
+//var_dump($array[0]);
+//echo count($array);
+
+$age["peter"] = "35";
+$age["Mahmoud"] = "26";
+$age["ben"] = "37";
+
+//echo gettype($age) . "\n";
+//echo "Peter is {$age["peter"]} years old.\n";
+//echo "Mahmoud is {$age["Mahmoud"]} years old.\n";
+//echo  "Ben is {$age["ben"]} years old.\n";
+
+$obj = new stdClass();
+
+//echo gettype($obj) . "\n";
+
+
+
+class Getter {
+    public $got = '';
+    public function __construct()
+    {
+        $this->got = "me";
+    }
+}
+
+$getter = new Getter();
+
+echo gettype(Getter::class) . "\n";
+
+//echo "result: " . $getter->got;
+
+
+
